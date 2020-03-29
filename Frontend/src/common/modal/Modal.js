f09440dd68f66f9bd05b85dc340ceb063941b478
Plekhanov <atagan@rambler.ru>
2020-03-29 12:21:53 +0300

    in progress

--- Frontend/src/common/modal/Modal.js
+++ Frontend/src/common/modal/Modal.js
@@ -1,46 +1,55 @@
 import React, { Component } from "react";
 import Portal from "./Portal";
 import './Modal.css'
+import PropTypes from 'prop-types';
 
 /**
+ * Модальное окео
  * https://github.com/YauhenKavalchuk/react-components/tree/12_modal
  */
-export default class Modal extends Component {
+const Modal = ({
+                   title, isOpen, onCancel, onSubmit, children,
+               }) => {
 
-    constructor(props) {
-        super(props);
+    return (
+        <>
+            { isOpen &&
+            <Portal>
+                <div className="modalOverlay">
+                    <div className="modalWindow">
+                        <div className="modalHeader">
+                            <div className="modalTitle">{title}</div>
 
-        this.title = props.title;
-        this.isOpen = props.isOpen;
-        this.onCancel = props.onCancel;
-        this.onSubmit = props.onSubmit;
-        this.children = props.children;
-    }
-
-
-    render() {
-        return (
-            <>
-                {this.isOpen &&
-                <Portal>
-                    <div className="modalOverlay">
-                        <div className="modalWindow">
-                            <div className="modalHeader">
-                                <div className="modalTitle">{this.title}</div>
-
-                            </div>
-                            <div className="modalBody">
-                                {this.children}
-                            </div>
-                            <div className="modalFooter">
-                                <button onClick={this.onCancel}>Cancel</button>
-                                <button onClick={this.onSubmit}>Submit</button>
-                            </div>
+                        </div>
+                        <div className="modalBody">
+                            {children}
+                        </div>
+                        <div className="modalFooter">
+                            <button onClick={onCancel}>Cancel</button>
+                            <button onClick={onSubmit}>Submit</button>
                         </div>
                     </div>
-                </Portal>
-                }
-            </>
-        );
-    }
-}
+                </div>
+            </Portal>
+            }
+        </>
+    );
+};
+
+Modal.propTypes = {
+    title: PropTypes.string,
+    isOpen: PropTypes.bool,
+    onCancel: PropTypes.func,
+    onSubmit: PropTypes.func,
+    children: PropTypes.node,
+};
+
+Modal.defaultProps = {
+    title: 'Modal title',
+    isOpen: false,
+    onCancel: () => {},
+    onSubmit: () => {},
+    children: null,
+};
+
+export default Modal;
