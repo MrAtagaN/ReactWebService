549cfa0f55be28d066790322069467bef78a856a
Plekhanov <atagan@rambler.ru>
2020-03-29 15:53:21 +0300

    in progress

---
+++ Backend/src/main/java/com/plekhanov/react_web_service/config/DbConfig.java
@@ -0,0 +1,39 @@
+package com.plekhanov.react_web_service.config;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DbConfig {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+
+    @Bean("SQLite")
+    public DataSource getDataSourceSqLite() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.sqlite.JDBC");
+        dataSource.setUrl(url);
+        dataSource.setUsername("");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate getJdbcTemplate(DataSource getDataSource) {
+        return new JdbcTemplate(getDataSource);
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate getNamedParameterJdbcTemplate(DataSource getDataSource) {
+        return new NamedParameterJdbcTemplate(getDataSource);
+    }
+}
