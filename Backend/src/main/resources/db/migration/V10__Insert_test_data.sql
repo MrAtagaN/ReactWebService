c8c179a26e133494c81b78d4f7a10865aaf92e42
Plekhanov <atagan@rambler.ru>
2020-04-04 02:30:25 +0300

    in progress

--- Backend/src/main/resources/db/migration/V10__Insert_test_data.sql
+++ Backend/src/main/resources/db/migration/V10__Insert_test_data.sql
@@ -1,5 +1,6 @@
 INSERT INTO users (id, username, password, account_non_expired, account_non_locked, credentials_non_expired, enabled)
 values (1, 'admin', '$2a$10$nWq3jS4Xui7hMVz4L1dUVuRYzY8DQsfWUROmXA627yd.wjm/FrD5G', true, true, true, true );
+-- зашифрованный пароль admin
 
 INSERT INTO authorities (user_id, role) values (1, 'ADMIN');
 
