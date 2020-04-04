aaa9373ae37adbeaf6bbb0000909e6cc6230c39d
Plekhanov <atagan@rambler.ru>
2020-04-04 04:24:21 +0300

    in progress

--- Backend/src/main/java/com/plekhanov/react_web_service/config/DbConfig.java
+++ Backend/src/main/java/com/plekhanov/react_web_service/config/DbConfig.java
@@ -7,8 +7,6 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import javax.sql.DataSource;
 
@@ -38,20 +36,5 @@ public class DbConfig {
         return configuration.buildSessionFactory(registry);
     }
 
-    //    @Bean
-//    public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
-//        HibernateTransactionManager manager = new HibernateTransactionManager();
-//        manager.setSessionFactory(sessionFactory);
-//        return manager;
-//    }
 
-    @Bean
-    public JdbcTemplate getJdbcTemplate(DataSource getDataSource) {
-        return new JdbcTemplate(getDataSource);
-    }
-
-    @Bean
-    public NamedParameterJdbcTemplate getNamedParameterJdbcTemplate(DataSource getDataSource) {
-        return new NamedParameterJdbcTemplate(getDataSource);
-    }
 }
