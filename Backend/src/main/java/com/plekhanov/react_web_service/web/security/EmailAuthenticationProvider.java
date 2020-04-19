3e3bdf77cb53c1f84241a29b1ae41cfd84dd3197
Plekhanov <atagan@rambler.ru>
2020-04-19 09:09:28 +0300

    in progress

--- Backend/src/main/java/com/plekhanov/react_web_service/web/security/EmailAuthenticationProvider.java
+++ Backend/src/main/java/com/plekhanov/react_web_service/web/security/EmailAuthenticationProvider.java
@@ -3,12 +3,12 @@ package com.plekhanov.react_web_service.web.security;
 import com.plekhanov.react_web_service.dao.UserDao;
 import com.plekhanov.react_web_service.entities.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import java.text.MessageFormat;
 
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
@@ -37,16 +37,28 @@ public class EmailAuthenticationProvider implements AuthenticationProvider {
 
         if (isNotBlank(username) && isNotBlank(password)) {
             final User user = userDao.findByEmail(username);
-            if (user != null
-                    && user.isEnabled()
-                    && user.isAccountNonExpired()
-                    && user.isAccountNonLocked()
-                    && user.isCredentialsNonExpired()
-                    && bCryptPasswordEncoder.matches(password, user.getPassword())) {
-                return new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            if (user != null) {
+                if (!user.isEnabled()) {
+                    throw new DisabledException(MessageFormat.format("User {0} disabled!", username));
+                }
+                if (!user.isAccountNonExpired()) {
+                    throw new AccountExpiredException(MessageFormat.format("User {0} account expired!", username));
+                }
+                if (!user.isAccountNonLocked()) {
+                    throw new LockedException(MessageFormat.format("User {0} account is locked!", username));
+                }
+                if (!user.isCredentialsNonExpired()) {
+                    throw new CredentialsExpiredException(MessageFormat.format("User {0} credentials is expired!", username));
+                }
+                String actualPassword = user.getPassword();
+                if (!bCryptPasswordEncoder.matches(password, actualPassword)) {
+                    throw new BadCredentialsException(MessageFormat.format("User {0} bad credentials!", username));
+                }
+                //после всех проверок, аутентифицируем пользователя
+                return new UsernamePasswordAuthenticationToken(user, actualPassword, user.getAuthorities());
             }
         }
-        throw new UsernameNotFoundException("User " + username + " not found!");
+        throw new UsernameNotFoundException(MessageFormat.format("User {0} not found!", username));
     }
 
 
