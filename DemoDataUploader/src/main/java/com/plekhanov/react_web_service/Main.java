a48d0c61464d8b287cff9215aa1121e2e4dfd1ad
Plekhanov <atagan@rambler.ru>
2020-08-09 06:31:31 +0300

    рефактор

--- DemoDataUploader/src/main/java/com/plekhanov/react_web_service/Main.java
+++ DemoDataUploader/src/main/java/com/plekhanov/react_web_service/Main.java
@@ -2,7 +2,6 @@ package com.plekhanov.react_web_service;
 
 import com.plekhanov.react_web_service.demoDataUploader.DemoDataUploader;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -13,7 +12,6 @@ import org.springframework.context.annotation.Bean;
 public class Main {
 
     public static void main(String[] args) {
-        //без сервера
         new SpringApplicationBuilder(Main.class)
                 .web(WebApplicationType.NONE)
                 .run(args);
