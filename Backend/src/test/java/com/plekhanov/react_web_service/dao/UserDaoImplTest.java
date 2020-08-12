ac204c2c402f3b8679e2c0a5564d588d26996e18
Plekhanov <atagan@rambler.ru>
2020-08-13 01:11:25 +0300

    test

--- Backend/src/test/java/com/plekhanov/react_web_service/dao/UserDaoImplTest.java
+++ Backend/src/test/java/com/plekhanov/react_web_service/dao/UserDaoImplTest.java
@@ -1,12 +1,13 @@
 package com.plekhanov.react_web_service.dao;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
-class UserDaoImplTest {
+public class UserDaoImplTest {
 
     @Autowired
     UserDao userDao;
