69a642a4c1c47acbabcd838cbf4fadf1738371a2
PlekhanovSA <SAPlekhanov@sberbank.ru>
2020-08-13 23:10:46 +0300

    frontend

--- DemoDataUploader/src/main/java/com/plekhanov/react_web_service/Main.java
+++ DemoDataUploader/src/main/java/com/plekhanov/react_web_service/Main.java
@@ -2,20 +2,23 @@ package com.plekhanov.react_web_service;
 
 import com.plekhanov.react_web_service.demoDataUploader.DemoDataUploader;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(Main.class)
+        ConfigurableApplicationContext run = new SpringApplicationBuilder(Main.class)
                 .web(WebApplicationType.NONE)
                 .lazyInitialization(true)
                 .run(args);
+        System.exit(SpringApplication.exit(run));
     }
 
 
