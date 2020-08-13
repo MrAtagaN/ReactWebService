fa2870fc627fc5495d586e47b6add522530f5dba
Plekhanov <atagan@rambler.ru>
2020-08-13 03:27:08 +0300

    test

--- Backend/src/test/java/com/plekhanov/react_web_service/dao/UserDaoImplTest.java
+++ Backend/src/test/java/com/plekhanov/react_web_service/dao/UserDaoImplTest.java
@@ -1,9 +1,19 @@
 package com.plekhanov.react_web_service.dao;
 
 
+import com.plekhanov.react_web_service.entities.User;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 @SpringBootTest
@@ -12,9 +22,17 @@ public class UserDaoImplTest {
     @Autowired
     UserDao userDao;
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @BeforeEach()
+    public void init() {
+
+    }
+
     @Test
     void saveOrUpdate() {
-        userDao.findByName("asd");
+
     }
 
     @Test
@@ -23,9 +41,14 @@ public class UserDaoImplTest {
 
     @Test
     void findByName() {
+        //jdbcTemplate.update("INSERT INTO public.users (username, email) VALUES ('Sergey', 'gmail@plekhanov.ru')");
+        List<User> found = userDao.findByName("Sergey");
+        assertEquals(found.size(), 1);
     }
 
     @Test
     void findByEmail() {
+        User found = userDao.findByEmail("gmail@plekhanov.ru");
+        assertNotNull(found);
     }
 }
--- DemoDataUploader/src/main/java/com/plekhanov/react_web_service/Main.java
+++ DemoDataUploader/src/main/java/com/plekhanov/react_web_service/Main.java
@@ -14,6 +14,7 @@ public class Main {
     public static void main(String[] args) {
         new SpringApplicationBuilder(Main.class)
                 .web(WebApplicationType.NONE)
+                .lazyInitialization(true)
                 .run(args);
     }
 
