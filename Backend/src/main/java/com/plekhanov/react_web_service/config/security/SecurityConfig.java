a83f317c981add4d978a3c0aebf4b9e5de7ab52a
Plekhanov <atagan@rambler.ru>
2020-08-30 01:57:25 +0300

    refactor Auth

--- Backend/src/main/java/com/plekhanov/react_web_service/config/security/SecurityConfig.java
+++ Backend/src/main/java/com/plekhanov/react_web_service/config/security/SecurityConfig.java
@@ -4,13 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.plekhanov.react_web_service.web.ApiResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.access.AccessDeniedHandler;
