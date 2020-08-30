5d30300c933c12878f0b191697c7f3773bcce83a
Plekhanov <atagan@rambler.ru>
2020-08-30 04:57:59 +0300

    refactor

--- Backend/src/main/java/com/plekhanov/react_web_service/utils/SecurityUtils.java
+++ Backend/src/main/java/com/plekhanov/react_web_service/utils/SecurityUtils.java
@@ -2,7 +2,6 @@ package com.plekhanov.react_web_service.utils;
 
 import com.plekhanov.react_web_service.entities.User;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class SecurityUtils {
@@ -13,12 +12,9 @@ public class SecurityUtils {
      * Получить текущего авторизованного пользователя
      */
     public static User getCurrentUser() {
-        final SecurityContext securityContext = SecurityContextHolder.getContext();
-        if (securityContext != null) {
-            final Authentication authentication = securityContext.getAuthentication();
-            if (authentication != null) {
-                return (User) authentication.getPrincipal();
-            }
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            return (User) authentication.getPrincipal();
         }
         return null;
     }
