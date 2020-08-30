951205d8e4e462b285895bef2ae71a1e7285f8ed
Plekhanov <atagan@rambler.ru>
2020-08-30 03:58:53 +0300

    refactor

---
+++ Backend/src/main/java/com/plekhanov/react_web_service/web/controllers/AuthController.java
@@ -0,0 +1,64 @@
+package com.plekhanov.react_web_service.web.controllers;
+
+import com.plekhanov.react_web_service.config.security.EmailPasswordAuthService;
+import com.plekhanov.react_web_service.config.security.JwtService;
+import com.plekhanov.react_web_service.entities.User;
+import com.plekhanov.react_web_service.web.ApiResponse;
+import com.plekhanov.react_web_service.web.dto.AuthenticationRequestDto;
+import com.plekhanov.react_web_service.web.dto.UserDto;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotNull;
+
+
+/**
+ * Эндпойнт для аутентификации
+ */
+@RestController
+@RequestMapping("/api/v1")
+@Validated
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class AuthController {
+
+    JwtService jwtService;
+    String jwtCookieName;
+    EmailPasswordAuthService emailPasswordAuthService;
+
+    public AuthController(final JwtService jwtService,
+                          final @Value("${jwt.cookie.name}") String jwtCookieName,
+                          final EmailPasswordAuthService emailPasswordAuthService) {
+        this.jwtService = jwtService;
+        this.jwtCookieName = jwtCookieName;
+        this.emailPasswordAuthService = emailPasswordAuthService;
+    }
+
+
+    /**
+     * login endpoint
+     *
+     * @param request email and password
+     * @return JWT token in cookie
+     */
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse<UserDto>> login(@RequestBody @NotNull final AuthenticationRequestDto request) {
+        final String email = request.getEmail();
+        final User user = emailPasswordAuthService.authenticate(email, request.getPassword());
+        final String jwtToken = jwtService.createJwtToken(email);
+        final HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("SET-COOKIE", jwtCookieName + "=" + jwtToken + "; HttpOnly; Path=/");
+        return ResponseEntity
+                .ok()
+                .headers(httpHeaders)
+                .body(ApiResponse.ok(UserDto.fromUser(user)));
+    }
+
+}
