11e0851645d92a067813b42e6c4c0af47bbd8631
Plekhanov <atagan@rambler.ru>
2021-12-26 14:21:44 +0300

    refactor

--- Backend/src/main/java/com/plekhanov/react_web_service/config/ConfigCron.java
+++ Backend/src/main/java/com/plekhanov/react_web_service/config/ConfigCron.java
@@ -1,5 +1,8 @@
 package com.plekhanov.react_web_service.config;
 
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
@@ -8,15 +11,26 @@ import java.util.concurrent.Executor;
 
 
 @Configuration
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class ConfigCron {
+
+
+    Integer poolSize;
+    String threadNamePrefix;
+
+    public ConfigCron(@Value("${taskExecutor.poolSize}") Integer poolSize,
+                      @Value("${taskExecutor.threadNamePrefix}") String threadNamePrefix) {
+        this.poolSize = poolSize;
+        this.threadNamePrefix = threadNamePrefix;
+    }
 
 
     @Bean
     public Executor taskExecutor() {
         ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
-        threadPoolTaskScheduler.setPoolSize(4); //TODO вынести в параметры
+        threadPoolTaskScheduler.setPoolSize(poolSize);
 
-        threadPoolTaskScheduler.setThreadNamePrefix("ThreadPoolTaskScheduler-"); //TODO вынести в параметры
+        threadPoolTaskScheduler.setThreadNamePrefix(threadNamePrefix);
         return threadPoolTaskScheduler;
     }
 }
