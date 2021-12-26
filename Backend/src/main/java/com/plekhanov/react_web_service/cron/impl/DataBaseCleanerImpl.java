b99e54a133e35de5c8abe253dbbd725e5fad288e
Plekhanov <atagan@rambler.ru>
2021-12-26 03:01:13 +0300

    refactor

--- Backend/src/main/java/com/plekhanov/react_web_service/cron/impl/DataBaseCleanerImpl.java
+++ Backend/src/main/java/com/plekhanov/react_web_service/cron/impl/DataBaseCleanerImpl.java
@@ -4,8 +4,8 @@ import com.plekhanov.react_web_service.cron.DataBaseCleaner;
 import com.plekhanov.react_web_service.dao.UserRegistrationDao;
 import com.plekhanov.react_web_service.model.entities.UserRegistrationRequest;
 import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -14,19 +14,26 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class DataBaseCleanerImpl implements DataBaseCleaner {
 
    UserRegistrationDao userRegistrationDao;
+   Integer userRegistrationRequestTimeLive;
 
 
-    @Scheduled(cron = "0 * * * * *", zone = "Europe/Istanbul") //TODO вынести в параметр
+    public DataBaseCleanerImpl(UserRegistrationDao userRegistrationDao,
+                               @Value("${dataBaseCleaner.userRegistrationRequestTimeLive}") Integer userRegistrationRequestTimeLive) {
+        this.userRegistrationDao = userRegistrationDao;
+        this.userRegistrationRequestTimeLive = userRegistrationRequestTimeLive;
+    }
+
+
+    @Scheduled(cron = "${dataBaseCleaner.cron}", zone = "${dataBaseCleaner.zone}")
     @Async
     public void cleanUserRegistrationRequest() {
         final List<UserRegistrationRequest> userRegistrationRequests = userRegistrationDao.findAll();
         userRegistrationRequests.stream()
-                .filter(request -> request.getCreationTime().isBefore(LocalDateTime.now().minusMinutes(5))) //TODO 5 вынести в параметр
+                .filter(request -> request.getCreationTime().isBefore(LocalDateTime.now().minusMinutes(userRegistrationRequestTimeLive))) //TODO 5 вынести в параметр
                 .forEach(userRegistrationDao::delete);
     }
 }
